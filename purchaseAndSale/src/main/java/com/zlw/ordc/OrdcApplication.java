66e99fbb1417db9f775b7527d86e631fcd7333e9
zhanglw <80243278@qq.com>
2020-04-14 21:25:06 +0800

    前台项目初始化

---
+++ purchaseAndSale/src/main/java/com/zlw/ordc/OrdcApplication.java
@@ -0,0 +1,13 @@
+package com.zlw.ordc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdcApplication.class, args);
+    }
+
+}
---
+++ purchaseAndSale/src/test/java/com/zlw/ordc/OrdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlw.ordc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
