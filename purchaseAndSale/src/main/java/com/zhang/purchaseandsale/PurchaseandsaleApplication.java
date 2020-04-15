f21df9f775cb850cff93b35bd30109f712363212
zhanglw <80243278@qq.com>
2020-04-15 20:01:48 +0800

    初始化

---
+++ purchaseAndSale/src/main/java/com/zhang/purchaseandsale/PurchaseandsaleApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.purchaseandsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseandsaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseandsaleApplication.class, args);
+    }
+
+}
